4fa881e03037908757a19a021e1e9eb42e559548
Emerson <ecampelo29@gmail.com>
2021-09-11 15:06:33 -0300

    fim da aula 2

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// conversão implícita
+
+const numero = 456
+const numeroString = "456"
+
+console.log('Usando == se compara o conteúdo da variável, porém há conversão implícita: ', numero == numeroString)
+
+console.log('Usando === se compara o conteúdo e o tipo da variável: ', numero === numeroString)
+
+console.log('Usando + o javascript irá concatenar os valores: ', numero + numeroString)
+
+
+// conversão explícita
+
+console.log('Usando + conversão explícita com Number somamos os valores: ', numero + Number(numeroString))
+
+console.log('Usando + conversão explícita com String concatenamos os valores: ', String(numero) + numeroString)
---
+++ variaveis.js
@@ -0,0 +1,34 @@
+// var -> primeira forma de se delcarar variáveis no js. 
+// permite o uso independente da posição da declaração, por isso deixou de ser usada 
+// para resolver esse problema da var foram criadas a let que permite trocar o valor da variável 
+// e a const, que não permite novas atribuições de valores. 
+
+const print = console.log
+let forma = 'retângulo'
+let altura = 5
+let comprimento = 7
+let area
+
+if (forma === 'retângulo') {
+    area = altura * comprimento
+} 
+else {
+    area = (altura * comprimento)/2
+}
+
+
+print(area)
+
+const formaConst = 'triângulo'
+const alturaConst = 5
+const comprimentoConst = 7
+let areaConst
+
+if (formaConst === 'retângulo') {
+    areaConst = alturaConst * comprimentoConst
+} 
+else {
+    areaConst = (alturaConst* comprimentoConst)/2
+}
+
+print(areaConst)
