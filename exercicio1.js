13f72b614f3a54ce226d76e5c90ed6811b0ce399
Emerson <ecampelo29@gmail.com>
2021-09-11 14:25:06 -0300

    pequena mudança no arquivo de exercicio1

--- exercicio1.js
+++ exercicio1.js
@@ -12,3 +12,12 @@ const resultado = idade +  dezAnosDepois
 console.log('nomeCompleto: ', nomeCompleto)
 console.log('resultado: ', resultado)
 console.log('Comparando bacharel com zero', bacharel == 0)
+
+
+// java é case sensitive e const não permite mudar o valor da variável, tipo o tuple do python:
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
