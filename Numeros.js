272c6aedf637824dee8916daca640b987e6cdcbb
Emerson <ecampelo29@gmail.com>
2021-09-11 14:18:24 -0300

    exemplos da aula 1

---
+++ Numeros.js
@@ -0,0 +1,13 @@
+const a = 10
+const b = 0
+console.log(a/b) // Infinity
+
+const c = 0
+console.log(c/b) // NaN 
+
+
+// comparações 
+
+console.log('Comparando a e b: ', a === b)
+
+console.log('Comparando c e b: ', c === b)
---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+const idade = 2
+const nome	= "Lara"
+const sobrenome	= "Ayumi"
+const bacharel	= false
+const dezAnosDepois	= "12"
+
+
+const nomeCompleto = nome + sobrenome 
+const resultado = idade +  dezAnosDepois
+
+
+console.log('nomeCompleto: ', nomeCompleto)
+console.log('resultado: ', resultado)
+console.log('Comparando bacharel com zero', bacharel == 0)
