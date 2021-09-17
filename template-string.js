79ceac41ace1262d5188677027a7346e429dcc64
emerson <ecampelo29@gmail.com>
2021-09-17 08:35:06 -0300

    aulas sobre funções, template string e ternários

---
+++ template-string.js
@@ -0,0 +1,10 @@
+const nome = "jão"
+const idade = 2021-1981
+const cidadeNascimento = 'São Paulo'
+
+const concatenacaoNormal = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci em "+ cidadeNascimento
+
+const concatenacaoTemplateString = `Meu nome é ${nome}, minha idade é ${idade} e nasci em ${cidadeNascimento}`
+
+console.log(concatenacaoNormal)
+console.log(concatenacaoNormal)
